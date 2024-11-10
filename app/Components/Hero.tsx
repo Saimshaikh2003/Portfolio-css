5fc06f4915c4f9ea0ad0b7c39b9a3768d8615f86
SAIM SHAIKH <saimshaikh0003@gmail.com>
2024-11-11 03:39:33 +0500

    portfoliocss

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,30 +1,26 @@
-
 import React from "react";
 import Navbar from "./Navbar";
 import "../Styles/hero.css";
 
-const Hero =  ()  => {
-    return(
-      <div id="hero"
-       className="hero-container"
-       >
-      <Navbar /> 
-      <div className="hero-content">
-        <div className="hidden lg:block"></div>
+const Hero = () => {
+  return (
+    <div id="hero">
+      <Navbar />
+      <div className="hero-container">
+        <div className="hero-content">
+          <div className="hidden lg:block"></div>
 
-
-        <div className="hero-text">
-          <div className="hero-msin">
-            <p data-aos="zoom-in-down">I AM</p>
-            <p data-aos="zoom-in-down">SAIM</p>
-            <p data-aos="zoom-in-down">SHAIKH</p>
+          <div className="hero-text">
+            <div className="hero-msin">
+              <p data-aos="zoom-in-down">I AM</p>
+              <p data-aos="zoom-in-down">SAIM</p>
+              <p data-aos="zoom-in-down">SHAIKH</p>
+            </div>
           </div>
         </div>
-        
-        </div>     
-       </div> 
-    );
-
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -49,3 +49,4 @@ const Projects = () => {
 };
 
 export default Projects;
+
