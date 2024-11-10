cf6c320ae9442b956c29fcf6ddf5e3ff667ab7f7
SAIM SHAIKH <saimshaikh0003@gmail.com>
2024-11-10 19:52:37 +0500

    portfoliocss

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -1,18 +1,71 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import "../Styles/card.css";
 interface propsType {
-    title : string;
-    desc :string;
-    img : string;
-    tags: string[];
+  title: string;
+  desc: string;
+  img: string;
+  tags: string[];
 }
 
-const Card:React.FC<propsType> = ({title , desc , img , tags}) => {
+const Card: React.FC<propsType> = ({ title, desc, img, tags }) => {
+  const [isClient, setIsClient] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    // Run only on the client side
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640);
+    };
+
+    // Initial check and event listener for resize
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  if (!isClient) return null;
+  // Initial check and event listener for resi
+
   return (
-    <div className={`Card ${window.innerWidth >= 640 ? 'card-sm' : ''}`} data-aos="zoom-in-down">
+    <div
+      className={`Card ${window.innerWidth >= 640 ? "card-sm" : ""}`}
+      data-aos="zoom-in-down"
+    >
+      <div>
+        <Image
+          className={`card-image ${!isSmallScreen ? "card-img-sm" : ""}`}
+          src={img}
+          width={350}
+          height={350}
+          alt={title}
+        />
+      </div>
+
+      <div className="card-content">
+        <div>{title}</div>
+        <div>{desc}</div>
+        <div className="tags">
+          {tags.map((tag, index) => (
+            <span className="tag" key={index}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+{
+  /*
+    <div className={`Card ${!isSmallScreen ? "card-sm" : ""}`} data-aos="zoom-in-down">
         <div>
-        <Image className = {`card-image ${window.innerWidth >=640 ? 'card-img-sm' : ''}`} 
+        className={`card-image ${!isSmallScreen ? "card-img-sm" : ""}`}
         src={img}
         width = {350}
         height = {350}
@@ -35,4 +88,5 @@ const Card:React.FC<propsType> = ({title , desc , img , tags}) => {
   );
 };
 
-export default Card;
+export default Card; */
+}
--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,21 @@ import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import AOS from "aos";
 
 export default function Home() {
   useEffect(() => {
-    AOS.init({
-      easing: "ease-out-back",
-      duration: 1200,
-      delay: 100,
-      mirror: true,
-      anchorPlacement: "bottom-bottom",
-      offset: 160,
-    });
-    AOS.refresh();
+    if (typeof window !== "undefined") {
+      import("aos").then((AOS) => {
+        AOS.init({
+          easing: "ease-out-back",
+          duration: 1200,
+          delay: 100,
+          mirror: true,
+          anchorPlacement: "bottom-bottom",
+          offset: 160,
+        });
+      });
+    }
   }, []);
   return (
     <main>
